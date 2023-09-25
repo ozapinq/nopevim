8fd4c3665ecf9d84b40c645c2e034149bb6ab1f4
Nikolay Sedelnikov <n.sedelnikov@gmail.com>
2023-09-25 15:02:45 +0200

    fix rust LSP

--- lua/config_lsp.lua
+++ lua/config_lsp.lua
@@ -6,4 +6,29 @@ local lsp = require('lspconfig')
 lsp.lua_ls.setup(coq.lsp_ensure_capabilities({}))
 lsp.pyright.setup(coq.lsp_ensure_capabilities({}))
 lsp.bashls.setup(coq.lsp_ensure_capabilities({}))
-lsp.rust_analyzer.setup(coq.lsp_ensure_capabilities({}))
+
+local on_attach = function(client)
+    require'completion'.on_attach(client)
+end
+
+lsp.rust_analyzer.setup(coq.lsp_ensure_capabilities({
+    on_attach=on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+}))
